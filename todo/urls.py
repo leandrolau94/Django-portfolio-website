04a6fd44d33f5e1bb93c46832fe688767e6554a4
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-07-27 16:33:47 -0400

    creating category detailed views and api functionality

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = "todo"
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>', views.CategoryDetailView.as_view(), name='detail'),
     path('create_category/', views.create_category, name='create_category'),
+    path('create_task/', views.create_task, name='create_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,24 +1,43 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.views import generic
 from django.urls import reverse
 from django.utils import timezone
 from .models import Category, Task
 
 # Create your views here.
-def index(request):
-    categories = Category.objects.all()
-    categories_label = [
-        "cars", "clothes", "food", "friends", "games", "health", "homework",
-        "insurance", "money", "sport", "study", "travel", "vacation", "work"
-    ]
-    context = {
-        "categories": categories,
-        "categories_label": categories_label
-    }
-    return render(request, 'todo/index.html', context)
+class IndexView(generic.ListView):
+    template_name = "todo/index.html"
+    context_object_name = "categories_and_labels"
+
+    def get_queryset(self):
+        categories = Category.objects.all()
+        categories_label = [
+            "cars", "clothes", "food", "friends", "games", "health", "homework",
+            "insurance", "money", "sport", "study", "travel", "vacation", "work"
+        ]
+        context = {
+            "categories": categories,
+            "categories_label": categories_label
+        }
+        return context
+
+class CategoryDetailView(generic.DetailView):
+    model = Category
+    template_name = "todo/category_detail.html"
 
 def create_category(request):
     new_category_name = request.POST['category']
     new_category = Category(category_name=new_category_name, pub_date=timezone.now())
     new_category.save()
     return HttpResponseRedirect(reverse('todo:index'))
+
+def create_task(request):
+    category = Category.objects.get(pk=request.POST['category_id'])
+    category.task_set.create(
+        task_description=request.POST['task_description'],
+        created_at=timezone.now()
+    )
+    return HttpResponseRedirect(reverse('todo:detail', args=(request.POST['category_id'],)))
