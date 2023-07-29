735c8d46cc36a03249ea58eb9cbd3e8e317d6232
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-07-29 00:09:47 -0400

    tests for views

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Category, Task
 
 # Create your tests here.
+def create_category(category_name):
+    """
+    Create a category with the given `category_name` and `date`.
+    """
+    time = timezone.now()
+    return Category.objects.create(category_name=category_name, pub_date=time)
+
+class CategoryIndexViewTest(TestCase):
+    pass
