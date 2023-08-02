a9b82b1757cb50721ef22eabde9dbc37f448c9dc
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-08-02 15:22:42 -0400

    finishing weather app logic

--- weather/views.py
+++ weather/views.py
@@ -5,10 +5,7 @@ from django.shortcuts import render
 from django.views import generic
 
 openweather_api_key = "786c40df863560a493ab62d34fe142b8"
-weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={72.3}&lon={45}&appid={openweather_api_key}"
-
-def generate_weather_url(lon, lat):
-    return f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={openweather_api_key}"
+weather_url = f"https://api.openweathermap.org/data/2.5/weather?appid={openweather_api_key}&lat={72.3}&lon={45}"
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -16,18 +13,26 @@ class IndexView(generic.ListView):
     template_name = 'weather/index.html'
 
     def get_queryset(self):
-        r = requests.get(weather_url)
-        # print(r.json())
-        data = r.json()
-        return data
+        try:
+            r = requests.get(weather_url)
+            # print(r.json())
+            data = r.json()
+            return data
+        except Exception as e:
+            return e
 
 def get_weather_info(request):
     lat = request.POST['lat']
     lon = request.POST['lon']
-    url = generate_weather_url(lon, lat)
-    r = requests.get(url)
-    data = r.json()
-    context = {
-        "weather": data,
-    }
-    return render(request, "weather/index.html", context)
+    url = f"https://api.openweathermap.org/data/2.5/weather?appid={openweather_api_key}"
+    payload = {'lat': lat, 'lon': lon}
+    try:
+        r = requests.get(url, params=payload)
+        data = r.json()
+        context = {
+            "weather": data,
+        }
+        # print(r.json())
+        return render(request, "weather/index.html", context)
+    except Exception as e:
+        return e
