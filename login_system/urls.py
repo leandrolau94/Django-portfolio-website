8ccace3cc331a520272fe8fa71f1f7e004084f8d
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-08-04 01:50:39 -0400

    beginning log in logic for login system app

--- login_system/urls.py
+++ login_system/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "login_system"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('log_in/', views.log_in, name='log_in'),
 ]
--- login_system/views.py
+++ login_system/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Wellcome to login system index page.")
+    return render(request, "login_system/index.html")
+
+def log_in(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    secret_phrase = request.POST['secret']
+    try:
+        user_account = get_object_or_404(User, user_name=username, password=password, secret_phrase=secret_phrase)
+        context = {
+            "user": user_account
+        }
+        if user_account:
+            return render(request, "login_system/account.html", context)
+    except User.DoesNotExist as e:
+        return e
