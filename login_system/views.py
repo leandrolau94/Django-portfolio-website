c8b5ac06d043856b078d3dad484a677ed769b8cb
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-08-04 23:47:51 -0400

    improving and finishing the log in button logic

--- login_system/views.py
+++ login_system/views.py
@@ -10,12 +10,14 @@ def log_in(request):
     username = request.POST['username']
     password = request.POST['password']
     secret_phrase = request.POST['secret']
-    try:
-        user_account = get_object_or_404(User, user_name=username, password=password, secret_phrase=secret_phrase)
+    if User.objects.filter(user_name=username, password=password, secret_phrase=secret_phrase).exists():
+        user_account = User.objects.get(user_name=username, password=password, secret_phrase=secret_phrase)
         context = {
             "user": user_account
         }
-        if user_account:
-            return render(request, "login_system/account.html", context)
-    except User.DoesNotExist as e:
-        return e
+        return render(request, "login_system/account.html", context)
+    else:
+        context = {
+            "error_message": "User Name, Password or Secret Phrase incorrect."
+        }
+        return render(request, "login_system/index.html", context)
