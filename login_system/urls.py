17ab96cb07f0f7acdbdf44a546083e8190168f2c
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-08-05 00:19:56 -0400

    making create account logic

--- login_system/urls.py
+++ login_system/urls.py
@@ -5,4 +5,6 @@ app_name = "login_system"
 urlpatterns = [
     path('', views.index, name='index'),
     path('log_in/', views.log_in, name='log_in'),
+    path('create_account/', views.create_account, name='create_account'),
+    path('create_new_account/', views.create_new_account, name='create_new_account'),
 ]
--- login_system/views.py
+++ login_system/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import User
 
 # Create your views here.
@@ -21,3 +22,21 @@ def log_in(request):
             "error_message": "User Name, Password or Secret Phrase incorrect."
         }
         return render(request, "login_system/index.html", context)
+
+def create_account(request):
+    return render(request, "login_system/create_account.html")
+
+def create_new_account(request):
+    new_username = request.POST['created_username']
+    new_password = request.POST['created_password']
+    new_secret_phrase = request.POST['created_secret']
+    if User.objects.filter(user_name=new_username).exists():
+        error_message = "User Name it's already been taken. Please tap a different one."
+        context = {
+            "error_message": error_message
+        }
+        return render(request, "login_system/create_account.html", context)
+    else:
+        new_account = User(user_name=new_username, password=new_password, secret_phrase=new_secret_phrase)
+        new_account.save()
+        return HttpResponseRedirect(reverse("login_system:index"))
