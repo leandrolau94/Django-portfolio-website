129345047010e49c50971193a551a595dd51c671
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-08-05 23:24:17 -0400

    finishing log in logic for schollms app, beginning the professor account logic dashboard

--- schoolms/urls.py
+++ schoolms/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'schoolms'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_start/', views.get_start, name='get_start'),
+    path('create_account/', views.create_account, name='create_account'),
+    path('login_page/', views.login_page, name='login_page'),
+    path('log_in/', views.log_in, name='log_in'),
 ]
--- schoolms/views.py
+++ schoolms/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
+from .models import Professor, Group, Student, Subject
 
 # Create your views here.
 def index(request):
     return render(request, "schoolms/index.html")
+
+def get_start(request):
+    return render(request, "schoolms/get_start.html")
+
+def create_account(request):
+    username = request.POST['username_prof']
+    email = request.POST['email_prof']
+    password = request.POST['password_prof']
+    if Professor.objects.filter(user_name=username).exists():
+        error_message = "Username has already been taken. Please enter a different one."
+        context = {
+            "error_message": error_message
+        }
+        return render(request, "schoolms/get_start.html", context)
+    else:
+        new_account = Professor(user_name=username, email=email, password=password)
+        new_account.save()
+        success_created = "Account succesfully created, please log in to continue."
+        context = {
+            "success_created": success_created,
+        }
+        return render(request, "schoolms/login_page.html", context)
+
+def login_page(request):
+    return render(request, "schoolms/login_page.html")
+
+def log_in(request):
+    user = request.POST['user_login']
+    password = request.POST['password_login']
+    if Professor.objects.filter(user_name=user, password=password).exists():
+        professor = Professor.objects.get(user_name=user, password=password)
+        context = {
+            "professor": professor
+        }
+        return render(request, "schoolms/account.html", context)
+    else:
+        error_login_msg = "Incorrect username or password. Please try again."
+        context = {
+            "error_login_msg": error_login_msg
+        }
+        return render(request, "schoolms/login_page.html", context)
